26ed91088ded3c49b9df4ac00487adb1e3f2268d
Pavel Perminov <pavel.perminov.23@gmail.com>
2013-03-11 00:28:28 +0200

    added one more field definition for fixing storeRelationability bug

--- sql/fix.storeRelationAbility.sql
+++ sql/fix.storeRelationAbility.sql
@@ -1,4 +1,4 @@
 ﻿UPDATE `field` SET `storeRelationAbility` = 'none' WHERE `storeRelationAbility` = '' AND `relation` = "0" AND `satellite` = "0";
 UPDATE `field` SET `storeRelationAbility` = 'one' WHERE `storeRelationAbility` = '' AND `relation` IN (2,3,5,7);
-UPDATE `field` SET `storeRelationAbility` = 'one' WHERE `storeRelationAbility` = '' AND `alias` IN  ("canStoreRelation","toggle","masterDimensionAlias","rowRequired","dependency","profileId");
+UPDATE `field` SET `storeRelationAbility` = 'one' WHERE `storeRelationAbility` = '' AND `alias` IN  ("canStoreRelation","toggle","masterDimensionAlias","rowRequired","dependency","profileId","proportions");
 UPDATE `field` SET `storeRelationAbility` = 'many' WHERE `storeRelationAbility` = '' AND `alias` = "profileIds";
