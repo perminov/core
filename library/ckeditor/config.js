05fa518ac60d7b15f932a138d5204ce7cfc68e4a
Pavel Perminov <pavel.perminov.23@gmail.com>
2013-03-10 04:25:10 +0200

    upload tabs of ckeditor dialogs disabled, if current cms user has no access to Save action

--- library/ckeditor/config.js
+++ library/ckeditor/config.js
@@ -2,11 +2,15 @@
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-
 CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	config.uiColor = '#dfe8f6';
 	config.skin = 'kama';
+	$.post(window.location.pathname.replace('form', 'save') + '?check', function(access){
+		if (access != 'ok') {
+			config.removeDialogTabs = 'link:upload;image:Upload;flash:Upload'
+		}
+	});
 };
