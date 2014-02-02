f92a4e1d23d56627f74679fd48d4d58509947757
Pavel Perminov <pavel.perminov.23@gmail.com>
2014-02-03 02:40:57 +0400

    -added undefined-check within setFilterValues at indi.action.index.js -added ability to setup login panel title by Indi.layout.options.loginPanelTitle -added submit-on-enter to login form -added handling if row has no 'title' property at breadCrumbsRowTitle() method at indi.trail.js -improved color detection within i-color-box inner html, added margin-left detection - all this for xls export

--- js/admin/indi.trail.js
+++ js/admin/indi.trail.js
@@ -39,7 +39,8 @@ var Indi = (function (indi) {
             this.breadCrumbsRowTitle = function(item){
 
                 // At first, we strip newline characters, html '<br>' tags
-                var title = item.row.title.replace(/[\n\r]/g, '').replace(/<br>/g, ' ');
+                var title = item.row.title ?
+                    item.row.title.replace(/[\n\r]/g, '').replace(/<br>/g, ' ') : (item.row._title || 'No title');
 
                 // Detect color
                 var colorDetected = title.match(/color[:=][ ]*[\'"]{0,1}([#a-zA-Z0-9]+)/i);
