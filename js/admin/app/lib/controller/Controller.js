c1c735dac239ded0cb326cbff06043af14f0a452
Pavel Perminov <pavel.perminov.23@gmail.com>
2015-02-28 16:41:18 +0400

    v.0.0.6

--- js/admin/app/lib/controller/Controller.js
+++ js/admin/app/lib/controller/Controller.js
@@ -30,50 +30,42 @@ Ext.define('Indi.lib.controller.Controller', {
     /**
      * Action dispatcher function
      *
-     * @param {String} action Action name/alias
-     * @param {String} uri The uri, that current action data was got by request to
+     * @param {Object} scope Object, containing `route`, `plain`, `uri` and `cfg` properties
      */
-    dispatch: function(action, uri, route, cfg) {
-        var me = this, actionExtendCmpName, actionCmpName;
+    dispatch: function(scope) {
+        var me = this, action = scope.route.last().action.alias, actionExtendCmpName, actionCmpName;
 
         // Setup `actions` property, for being a storage for action classes instances
         me.actions = me.actions || {};
 
         // Convert route items to Indi.trail.Item instances
-        route.forEach(function(r, i, a){
-            a[i] = Ext.create('Indi.trail.Item', r);
-        });
+        scope.route.forEach(function(r, i, a) {a[i] = Ext.create('Indi.trail.Item', r);});
 
         // Setup mode and view for current trail item action
-        if (!route.last().action.mode) route.last().action.mode = Indi.Controller.defaultMode[action];
-        if (!route.last().action.view) route.last().action.view = Indi.Controller.defaultView[action];
+        if (!scope.route.last().action.mode) scope.route.last().action.mode = Indi.Controller.defaultMode[action];
+        if (!scope.route.last().action.view) scope.route.last().action.view = Indi.Controller.defaultView[action];
 
         // Build the action parent component name
         actionExtendCmpName = 'Indi.Controller.Action';
-        if (route.last().action.mode) actionExtendCmpName += '.' + Indi.ucfirst(route.last().action.mode);
-        if (route.last().action.view) actionExtendCmpName += '.' + Indi.ucfirst(route.last().action.view.replace('_', '.'));
+        if (scope.route.last().action.mode) actionExtendCmpName += '.' + Indi.ucfirst(scope.route.last().action.mode);
+        if (scope.route.last().action.view) actionExtendCmpName += '.' + Indi.ucfirst(scope.route.last().action.view.replace('_', '.'));
 
         // Build the action component name
-        actionCmpName = 'Indi.controller.' + route.last().section.alias + '.action.' + route.last().action.alias;
+        actionCmpName = 'Indi.controller.' + scope.route.last().section.alias + '.action.' + scope.route.last().action.alias;
 
         // Define the action component
         Ext.define(actionCmpName, Ext.merge({extend: actionExtendCmpName}, me.actionsConfig[action]));
 
         // Build the id for action object
-        var id = 'i-section-' + route.last().section.alias + '-action-' + route.last().action.alias;
-        if (route.last().row) {
-            id += '-row-' + (route.last().row.id || 0);
-        } else if (route.last(1).row) {
-            id += '-parentrow-' + route.last(1).row.id;
+        scope.id = 'i-section-' + scope.route.last().section.alias + '-action-' + scope.route.last().action.alias;
+        if (scope.route.last().row) {
+            scope.id += '-row-' + (scope.route.last().row.id || 0);
+        } else if (scope.route.last(1).row) {
+            scope.id += '-parentrow-' + scope.route.last(1).row.id;
         }
 
         // Create action component instance, related to current action
-        me.actions[action] = Ext.create(actionCmpName, {
-            id: id,
-            route: route,
-            uri: Ext.clone(uri),
-            cfg: Ext.clone(cfg)
-        });
+        me.actions[action] = Ext.create(actionCmpName, scope);
     },
 
     // @inheritdoc
