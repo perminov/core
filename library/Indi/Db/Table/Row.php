fdad241273f5d37b1b10ca036e63135e91bb34c6
Pavel Perminov <pavel.perminov.23@gmail.com>
2015-12-16 15:45:25 +0300

    *_Row->_mismatch['#model'] setup, caused by dir-permissions is now performed only in case if filefields contents are truly going to be altered

--- library/Indi/Db/Table/Row.php
+++ library/Indi/Db/Table/Row.php
@@ -2695,7 +2695,7 @@ class Indi_Db_Table_Row implements ArrayAccess
         if (is_array($fields) && !count($fields)) return;
 
         // If value, got by $this->model()->dir() call, is not a directory name
-        if (!Indi::rexm('dir', $dir = $this->model()->dir())) {
+        if ((is_array($fields) ?: $this->_files) && !Indi::rexm('dir', $dir = $this->model()->dir())) {
 
             // Assume it is a error message, and put it under '#model' key within $this->_mismatch property
             $this->_mismatch['#model'] = $dir;
