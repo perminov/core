bd519af5989438ba66f35be3259741718599302f
Pavel Perminov <pavel.perminov.23@gmail.com>
2016-01-05 06:18:42 +0300

    consistensy for filters now can be switched off

--- library/Indi/View/Helper/FilterCombo.php
+++ library/Indi/View/Helper/FilterCombo.php
@@ -61,6 +61,9 @@ class Indi_View_Helper_FilterCombo extends Indi_View_Helper_FormCombo {
      */
     public function getConsistence() {
 
+        // Check if consistency is not toggled Off for current filter
+        if ($this->filter->model()->fields('consistensy') && !$this->filter->consistensy) return;
+
         // If filter is non-boolean
         if ((($relation = $this->getField()->relation) || $this->getField()->columnTypeId == 12) && Indi::uri('format')) {
 
