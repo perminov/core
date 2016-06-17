dbb72727fcaa772332b508ecc5cc96ffbca7e5b2
Pavel Perminov <pavel.perminov.23@gmail.com>
2016-06-17 21:35:33 +0300

    added ^ E_STRICT into error_reporting() call

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ define('DOC', rtrim($_SERVER['DOCUMENT_ROOT'], '/'));
 define('URI', rtrim($_SERVER['REQUEST_URI'], '/'));
 
 // Set up error reporting
-error_reporting(E_ALL^E_NOTICE);
+error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
 ini_set('display_errors', 'On');
 
 // Set include path. Here we add more include paths, in case if some stuff is related to front module only,
