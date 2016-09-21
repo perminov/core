d605ac71b559429c6622f04b26fb2f115f2f9952
Pavel Perminov <pavel.perminov.23@gmail.com>
2016-09-21 19:41:48 +0300

    added gridColumn$ProfileIds: {editor: true} in /sectionActions

---
+++ js/admin/app/controller/sectionActions.js
@@ -0,0 +1,11 @@
+Ext.define('Indi.controller.sectionActions', {
+    extend: 'Indi.lib.controller.Controller',
+    actionsConfig: {
+        index: {
+            gridColumn$ProfileIds: {editor: true}
+        },
+        form: {
+
+        }
+    }
+});
