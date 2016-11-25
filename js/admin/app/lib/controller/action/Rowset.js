3ffabbe42544ce29e806609df6451d7d74b3bb55
Pavel Perminov <pavel.perminov.23@gmail.com>
2016-11-25 05:18:03 +0300

    1. Now filters having {disabled: true} will be ignored while building rowset's load query string only if their fields have a satellites 2. Added btn-icon-editor.png

--- js/admin/app/lib/controller/action/Rowset.js
+++ js/admin/app/lib/controller/action/Rowset.js
@@ -149,7 +149,7 @@ Ext.define('Indi.lib.controller.action.Rowset', {
         for (var i = 0; i < filterCmpA.length; i++) {
 
             // We do not involve values of hidden or disabled filter components in request query building
-            if (filterCmpA[i].hidden || filterCmpA[i].disabled) continue;
+            if (filterCmpA[i].hidden || (filterCmpA[i].disabled && filterCmpA[i].field && filterCmpA[i].field.satellite)) continue;
 
             // Define a shortcut for filter filed alias
             var alias = filterCmpA[i].name;
