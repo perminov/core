dbf6eccc704df8422573215dc91ae64c29e306b3
root <root@prazdnik.life>
2016-11-29 23:11:29 +0300

    added matchSql into eval fields

--- application/models/Notice.php
+++ application/models/Notice.php
@@ -7,4 +7,10 @@ class Notice extends Indi_Db_Table {
      * @var string
      */
     public $_rowClass = 'Notice_Row';
+
+    /**
+     * Array of fields, which contents will be evaluated with php's eval() function
+     * @var array
+     */
+    protected $_evalFields = array('matchSql');
 }
