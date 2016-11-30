e64ac66a990542ee6c98a09f7946a117f9472711
Pavel Perminov <pavel.perminov.23@gmail.com>
2016-11-30 04:30:42 +0300

    added NoticeGetter model

---
+++ application/models/NoticeGetter/Row.php
@@ -0,0 +1,13 @@
+<?php
+class NoticeGetter_Row extends Indi_Db_Table_Row {
+
+    /**
+     * @return int
+     */
+    public function save(){
+
+
+        // Standard save
+        return parent::save();
+    }
+}
