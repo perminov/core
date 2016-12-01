35cdfa663a9330cf61252baceebea6b0a4604980
Pavel Perminov <pavel.perminov.23@gmail.com>
2016-12-01 05:23:45 +0300

    notices can now be sent on emails

--- application/models/NoticeGetter/Row.php
+++ application/models/NoticeGetter/Row.php
@@ -12,24 +12,37 @@ class NoticeGetter_Row extends Indi_Db_Table_Row {
     public function ar($row) {
 
         // If criteria, that recipients should match - is not specified
-        if (!strlen($this->criteria)) return true;
+        if (!strlen($this->criteria) && $this->mail == 'n') return true;
 
-        // Assign `row` prop, that will be visible in compiling context
-        $this->row = $row;
+        // Start building WHERE clauses array
+        $where = array('`toggle` = "y"');
 
-        // Unset previously compiled criteria
-        unset($this->_compiled['criteria']);
-
-        // Find the name of database table, where recipients should be found
+        // Find the name of database table, where recipients should be found within
         foreach (Indi_Db::role() as $profileIds => $entityId)
             if (in($this->profileId, $profileIds))
                 if ($table = Indi::model($entityId)->table())
                     break;
 
+        // Prevent recipients duplication
+        if ($table == 'admin') $where[] = '`profileId` = "' . $this->profileId . '"';
+
+        // If criteria specified
+        if (strlen($this->criteria)) {
+
+            // Assign `row` prop, that will be visible in compiling context
+            $this->row = $row;
+
+            // Unset previously compiled criteria
+            unset($this->_compiled['criteria']);
+
+            // Append compiled criteria to WHERE clauses array
+            if (strlen($criteria = $this->compiled('criteria'))) $where[] = '(' . $criteria . ')';
+        }
+
         // Recompile criteria, use it for finding recipients ids
         $return = Indi::db()->query($sql = '
-            SELECT `id` FROM `' . $table . '` WHERE ' . $this->compiled('criteria')
-        )->fetchAll(PDO::FETCH_COLUMN, 0);
+            SELECT `id`, `email`  FROM `' . $table . '` WHERE ' . im($where, ' AND ')
+        )->fetchAll(PDO::FETCH_KEY_PAIR);
 
         // Return array of found recipients ids
         return $return;
