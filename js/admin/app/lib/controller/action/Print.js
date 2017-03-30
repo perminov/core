111ef4afbb24ed07deda4d4d998eb8c3f19e5d70
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-03-30 19:39:09 +0300

    added Indi.std to iframe src in print-actions

--- js/admin/app/lib/controller/action/Print.js
+++ js/admin/app/lib/controller/action/Print.js
@@ -146,7 +146,7 @@ Ext.define('Indi.lib.controller.action.Print', {
         if (!item$) return false;
 
         // Get iframe src, either from item$'s `field` prop, if item$ is an object
-        if (Ext.isObject(item$) && item$.field) src = me.ti().row[item$.field];
+        if (Ext.isObject(item$) && item$.field) src = Indi.std + me.ti().row[item$.field];
 
         // Or from item$ itself, if it is a string
         else if (Ext.isString(item$)) src = item$;
