840eef9ba2dfe9acbdb4349b9bc562b992dad145
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-04-27 07:31:38 +0300

    windows --

--- js/admin/app/override/Ext.ZIndexManager.js
+++ js/admin/app/override/Ext.ZIndexManager.js
@@ -45,3 +45,49 @@ Ext.override(Ext.ZIndexManager, {
         me._hideModalMask();
     }
 });
+
+/**
+ * Added ability to configure overflow menu, by passing object-value as an enableOverflow сап param, instead of bool-value
+ */
+Ext.override(Ext.layout.container.boxOverflow.Menu, {
+
+    // We don't define a prefix in menu overflow.
+    getSuffixConfig: function() {
+        var me = this,
+            layout = me.layout,
+            oid = layout.owner.id,
+            menuCfg = Ext.isSimpleObject(layout.owner.enableOverflow) ? layout.owner.enableOverflow : {};
+
+        /**
+         * @private
+         * @property {Ext.menu.Menu} menu
+         * The expand menu - holds items for every item that cannot be shown
+         * because the container is currently not large enough.
+         */
+        me.menu = new Ext.menu.Menu(Ext.merge({
+            listeners: {
+                scope: me,
+                beforeshow: me.beforeMenuShow
+            }
+        }, menuCfg));
+
+        /**
+         * @private
+         * @property {Ext.button.Button} menuTrigger
+         * The expand button which triggers the overflow menu to be shown
+         */
+        me.menuTrigger = new Ext.button.Button({
+            id      : oid + '-menu-trigger',
+            cls     : Ext.layout.container.Box.prototype.innerCls + ' ' + me.triggerButtonCls,
+            hidden  : true,
+            ownerCt : layout.owner, // To enable the Menu to ascertain a valid zIndexManager owner in the same tree
+            ownerLayout: layout,
+            iconCls : Ext.baseCSSPrefix + me.getOwnerType(layout.owner) + '-more-icon',
+            ui      : layout.owner instanceof Ext.toolbar.Toolbar ? 'default-toolbar' : 'default',
+            menu    : me.menu,
+            getSplitCls: function() { return '';}
+        });
+
+        return me.menuTrigger.getRenderTree();
+    }
+});
