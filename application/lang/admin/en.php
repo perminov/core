8f9a68cf84f2c0c9461ea43594f914c7c4ffee7e
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-08-09 22:54:52 +0300

    1.added I_(MCHECK|JCHECK)_EQL constants in application/lang/(ru|en).php 2.added non-zero check before checking 'rex' rule in *_Row->mcheck(), both for 'field' and 'non-field' modes 3.added 3rd arg $fn to jcheck() fn declaration in func.php 4.added support for new 'eql' (mean equal) rule to jcheck() fn declaration in func.php

--- application/lang/admin/en.php
+++ application/lang/admin/en.php
@@ -209,6 +209,8 @@ define('I_DEMO_ACTION_OFF', 'This action is turned Off in demo-mode');
 define('I_MCHECK_REQ', 'Field "%s" - is required');
 define('I_MCHECK_REG', 'Value "%s" of field "%s" - is in invalid format');
 define('I_MCHECK_KEY', 'No object of type "%s" was found by key "%s"');
+define('I_MCHECK_EQL', 'Wrong value');
 define('I_JCHECK_REQ', 'Param "%s" - is not given');
 define('I_JCHECK_REG', 'Value "%s" of param "%s" - is in invalid format');
 define('I_JCHECK_KEY', 'No object of type "%s" was found by key "%s"');
+define('I_JCHECK_EQL', 'Wrong value');
