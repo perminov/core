41d11f15c525bab003348068e08601c9517d6f20
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-08-09 18:22:28 +0300

    1.improved daily working hours extension-for-existing-events logic in action/Calendar.js 2.added Db/Table/Row/Schedule.php:daily() method, that disable daily working hours bounds for existing event

--- library/Indi/Db/Table/Schedule.php
+++ library/Indi/Db/Table/Schedule.php
@@ -15,42 +15,6 @@ class Indi_Db_Table_Schedule extends Indi_Db_Table {
     );
 
     /**
-     * @var string
-     */
-    protected $_sinceField = 'spaceSince';
-
-    /**
-     * @var string
-     */
-    protected $_untilField = 'spaceUntil';
-
-    /**
-     * @var string
-     */
-    protected $_frameField = 'spaceFrame';
-
-    /**
-     * @return string
-     */
-    public function sinceField() {
-        return $this->_sinceField;
-    }
-
-    /**
-     * @return string
-     */
-    public function untilField() {
-        return $this->_untilField;
-    }
-
-    /**
-     * @return string
-     */
-    public function frameField() {
-        return $this->_frameField;
-    }
-
-    /**
      * Set/get for $this->_daily
      */
     public function daily($arg1 = false, $arg2 = false) {
