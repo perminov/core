357a19dcfb5e34274635457aa28f6deabebba904
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-09-21 18:38:45 +0300

    1.added Indi::l10n($text, $to, $from) definition. Currently it works only for ru->en and en->ru using transliteration 2.added Indi::l10n() usage in Indi_Db_Table->insert() and Field_Row->toggleL10n

--- library/Indi.php
+++ library/Indi.php
@@ -1342,6 +1342,86 @@ class Indi {
 	}
 
     /**
+     * Try to localize using transliteration
+     *
+     * @static
+     * @param $text
+     * @param $to
+     * @param null $from
+     * @return mixed
+     */
+    public static function l10n($text, $to, $from = null) {
+
+        // If $from arg is not given - set it to be same as Indi::ini('lang')->admin
+        if (!$from) $from = Indi::ini('lang')->admin;
+
+        // If given lang is same as current lang return $text as is
+        if ($to == $from) return $text;
+
+        // If we need to convert from Russian to English
+        if ($from == 'ru' && $to == 'en') {
+
+            // Symbols
+            $ru = array(
+                'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п',
+                'р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
+                'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П',
+                'Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'
+            );
+
+            // Replacements
+            $en = array(
+                'a','b','v','g','d','e','yo','zh','z','i','i','k','l','m','n','o','p',
+                'r','s','t','u','f','h','c','ch','sh','shh','','y','','e','yu','ya',
+                'A','B','V','G','D','E','Yo','Zh','Z','I','I','K','L','M','N','O','P',
+                'R','S','T','U','F','H','C','Ch','Sh','Shh','','Y','','E','Yu','Ya',
+            );
+
+            // Combine
+            $ex = array_combine($ru, $en);
+
+            // Replace
+            return preg_replace_callback('/('. im($ru, '|') .')/', function($m) use ($ex) {
+                return $ex[$m[1]];
+            }, $text);
+
+        // If we need to convert from English to Russian
+        } else if ($from == 'en' && $to == 'ru') {
+
+            // Replacements
+            $en = array(
+                'sh','shh',/*'',*/'uy',/*'',*/'ye','yu','ya','j','sch','sck',
+                'a','b','v','g','d','e','yo','zh','z','i','y','k','l','m','n','o','p',
+                'r','s','t','u','f','h','c','ch',
+                'Sh','Shh',/*'',*/'Uy',/*'',*/'Ye','Yu','Ya','J','Sch','Sck',
+                'A','B','V','G','D','E','Yo','Zh','Z','I','Y','K','L','M','N','O','P',
+                'R','S','T','U','F','H','C','Ch',
+            );
+
+            // Symbols
+            $ru = array(
+                'ш','щ',/*'ъ',*/'ы',/*'ь',*/'э','ю','я','дж','шк','шк',
+                'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п',
+                'р','с','т','у','ф','х','ц','ч',
+
+                'Ш','Щ',/*'Ъ',*/'Ы',/*'Ь',*/'Э','Ю','Я','Дж','Шк','Шк',
+                'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П',
+                'Р','С','Т','У','Ф','Х','Ц','Ч',
+            );
+
+            // Combine
+            $ex = array_combine($en, $ru);
+
+            // Replace
+            return preg_replace_callback('/('. im($en, '|') .')/', function($m) use ($ex) {
+                return $ex[$m[1]];
+            }, $text);
+
+        // Else return as is
+        } else return $text;
+    }
+
+    /**
      * Converts an html color name to a hex color value
      *
      * @static
