0cd7eb07d3ef3c2de16143c513cab94c051a623b
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-12-26 03:46:24 +0300

    added Month and Year model files into application/models

---
+++ application/models/Month/Row.php
@@ -0,0 +1,58 @@
+<?php
+class Month_Row extends Indi_Db_Table_Row {
+
+    /**
+     * @return int
+     */
+    public function save(){
+
+        // Build title
+        $this->title = $this->foreign('month')->title . ' ' . $this->foreign('yearId')->title;
+
+        // Standard save
+        return parent::save();
+    }
+
+    /**
+     * 'Ym' here mean date('Y-m')
+     *
+     * @return string
+     */
+    public function Ym() {
+        return $this->foreign('yearId')->title . '-' . $this->month;
+    }
+
+    /**
+     * Get count of a certain weekdays within the current month
+     *
+     * @param string $wd
+     * @return mixed
+     */
+    public function wdQty($wd = 'Mon') {
+
+        // Pick 4-digit year from `title`
+        list($empty, $y) = explode(' ', $this->title);
+
+        // Get timestamp, related to current month's first day
+        $time = strtotime($y . '-' . $this->month . '-01');
+
+        // While we a not jumped to the next month
+        while (date('m', $time) == $this->month) {
+
+            // If current timestamp points to weekday same as $wd arg
+            if (date('D', $time) == $wd) {
+
+                // Increase $qty counter
+                $qty ++;
+
+                // Jump to next week
+                $time += 60 * 60 * 24 * 7;
+
+            // Else jump to next day
+            } else $time += 60 * 60 * 24;
+        }
+
+        // Return
+        return $qty;
+    }
+}
