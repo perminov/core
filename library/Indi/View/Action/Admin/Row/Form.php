376f7d327fc73284c75fdef56e706411e5a0200d
Pavel Perminov <pavel.perminov.23@gmail.com>
2018-06-22 02:32:22 +0300

    1.refactored View/Action/Admin/Row/Form.php 2.deleted View/Helper/Admin/FormUpload.php

--- library/Indi/View/Action/Admin/Row/Form.php
+++ library/Indi/View/Action/Admin/Row/Form.php
@@ -2,27 +2,21 @@
 class Indi_View_Action_Admin_Row_Form extends Indi_View_Action_Admin_Row {
     public function render() {
 
-        // Start output buffering
-        ob_start();
-
-        // Declare arrays that will store info about what fields should be excluded
-        // from form, and what included, but disabled in form
-        $disabled = array(); $excluded = array();
-        foreach (Indi::trail()->disabledFields as $disabledField) {
-            if ($disabledField->displayInForm) $disabled[$disabledField->fieldId] = true;
-            else $excluded[$disabledField->fieldId] = true;
-        }
+        // Declare arrays that will store info about what fields will be excluded from form
+        $excluded = array();
+        foreach (Indi::trail()->disabledFields as $disabledField)
+            if (!$disabledField->displayInForm)
+                $excluded[$disabledField->fieldId] = true;
 
         // Echo a <tr> for each form's field, but only if field's control element's 'hidden' checkbox is not checked
         foreach (Indi::trail()->fields as $fieldR)
-            if (!$excluded[$fieldR->id] && $fieldR->foreign('elementId')->hidden != 1) {
-                if ($fieldR->foreign('elementId')->alias == 'upload') {
-                    echo Indi::view()->formUpload($fieldR->alias, null, 'extjs');
-                } else if (preg_match('/combo|radio|multicheck/', $fieldR->foreign('elementId')->alias))
+            if (!$excluded[$fieldR->id] && $fieldR->foreign('elementId')->hidden != 1)
+                if (preg_match('/combo|radio|multicheck/', $fieldR->foreign('elementId')->alias))
                     Indi::view()->formCombo($fieldR->alias);
-            }
+                else if ($fieldR->foreign('elementId')->alias == 'upload' && t()->row->abs($fieldR->alias))
+                    t()->row->view($fieldR->alias, t()->row->file($fieldR->alias));
 
-        // Return buffered output with parent's return-value
-        return ob_get_clean() . parent::render();
+        // Return parent's return-value
+        return parent::render();
     }
 }
