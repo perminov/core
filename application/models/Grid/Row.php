0333701ea4d8c4399d1be307898fac8b6e9fcb7a
Pavel Perminov <pavel.perminov.23@gmail.com>
2018-07-04 23:27:58 +0300

    1.added /sections/export/id/xxx/ 2.added Grid_Row->_ctor(), Grid_Row->export() 3.added Section_Row_Base->_ctor(), Section_Row_Base->export() 4.added Section2action_Row->_ctor(), Section2action_Row->export()

--- application/models/Grid/Row.php
+++ application/models/Grid/Row.php
@@ -53,4 +53,55 @@ class Grid_Row extends Indi_Db_Table_Row {
         // Call parent
         return parent::move($direction, $within);
     }
+
+    /**
+     * Build a string, that will be used in Grid_Row->export()
+     *
+     * @return string
+     */
+    protected function _ctor() {
+
+        // Use original data as initial ctor
+        $ctor = $this->_original;
+
+        // Exclude `id` and `move` as they will be set automatically by MySQL and Indi Engine, respectively
+        unset($ctor['id'], $ctor['move']);
+
+        // Exclude props that are already represented by one of shorthand-fn args
+        foreach (ar('sectionId,fieldId') as $arg) unset($ctor[$arg]);
+
+        // Foreach $ctor prop
+        foreach ($ctor as $prop => &$value) {
+
+            // Get field
+            $field = Indi::model('Grid')->fields($prop);
+
+            // Exclude prop, if it has value equal to default value
+            if ($field->defaultValue == $value) unset($ctor[$prop]);
+
+            // Else if prop contains keys - use aliases instead
+            else if ($field->storeRelationAbility != 'none') {
+                if ($prop == 'gridId') {
+                    $value = ($_ = $this->foreign('gridId')) && $_->fieldId ? $_->foreign('fieldId')->alias : $_->alias;
+                }
+            }
+        }
+
+        // Stringify and return $ctor
+        return var_export($ctor, true);
+    }
+
+    /**
+     * Build an expression for creating the current grid column in another project, running on Indi Engine
+     *
+     * @return string
+     */
+    public function export() {
+
+        // Return creation expression
+        return "grid('" .
+            $this->foreign('sectionId')->alias . "','" .
+            $this->foreign('fieldId')->alias . "', " .
+            $this->_ctor() . ");";
+    }
 }
