b1d3dbb4f03b1837a49a7a0524c924d269479e7c
Pavel Perminov <pavel.perminov.23@gmail.com>
2018-07-13 05:00:13 +0300

    PHPWord/Template.php: added ability to clone table rows

--- library/PHPWord/Template.php
+++ library/PHPWord/Template.php
@@ -104,5 +104,113 @@ class PHPWord_Template {
         
         rename($this->_tempFileName, $strFilename);
     }
+
+    /**
+     * Clone a table row in a template document.
+     *
+     * @param string $search
+     * @param integer $numberOfClones
+     *
+     * @return void
+     *
+     * @throws \PhpOffice\PhpWord\Exception\Exception
+     */
+    public function cloneRow($search, $numberOfClones)
+    {
+        $search = 'v' . ucfirst($search);
+        $tagPos = strpos($this->_documentXML, $search);
+        if (!$tagPos) {
+            throw new Exception("Can not clone row, template variable not found or variable contains markup.");
+        }
+
+        $rowStart = $this->findRowStart($tagPos);
+        $rowEnd = $this->findRowEnd($tagPos);
+        $xmlRow = $this->getSlice($rowStart, $rowEnd);
+
+        // Check if there's a cell spanning multiple rows.
+        if (preg_match('#<w:vMerge w:val="restart"/>#', $xmlRow)) {
+            // $extraRowStart = $rowEnd;
+            $extraRowEnd = $rowEnd;
+            while (true) {
+                $extraRowStart = $this->findRowStart($extraRowEnd + 1);
+                $extraRowEnd = $this->findRowEnd($extraRowEnd + 1);
+
+                // If extraRowEnd is lower then 7, there was no next row found.
+                if ($extraRowEnd < 7) {
+                    break;
+                }
+
+                // If tmpXmlRow doesn't contain continue, this row is no longer part of the spanned row.
+                $tmpXmlRow = $this->getSlice($extraRowStart, $extraRowEnd);
+                if (!preg_match('#<w:vMerge/>#', $tmpXmlRow) &&
+                    !preg_match('#<w:vMerge w:val="continue" />#', $tmpXmlRow)) {
+                    break;
+                }
+                // This row was a spanned row, update $rowEnd and search for the next row.
+                $rowEnd = $extraRowEnd;
+            }
+            $xmlRow = $this->getSlice($rowStart, $rowEnd);
+        }
+
+        $result = $this->getSlice(0, $rowStart);
+        for ($i = 1; $i <= $numberOfClones; $i++) {
+            $result .= preg_replace('/v([A-Z][a-z]*)+/', '\\0#' . $i, $xmlRow);
+        }
+        $result .= $this->getSlice($rowEnd);
+
+        $this->_documentXML = $result;
+    }
+
+    /**
+     * Find the start position of the nearest table row before $offset.
+     *
+     * @param integer $offset
+     *
+     * @return integer
+     *
+     * @throws Exception
+     */
+    protected function findRowStart($offset)
+    {
+        $rowStart = strrpos($this->_documentXML, '<w:tr ', ((strlen($this->_documentXML) - $offset) * -1));
+
+        if (!$rowStart) {
+            $rowStart = strrpos($this->_documentXML, '<w:tr>', ((strlen($this->_documentXML) - $offset) * -1));
+        }
+        if (!$rowStart) {
+            throw new Exception('Can not find the start position of the row to clone.');
+        }
+
+        return $rowStart;
+    }
+
+    /**
+     * Find the end position of the nearest table row after $offset.
+     *
+     * @param integer $offset
+     *
+     * @return integer
+     */
+    protected function findRowEnd($offset)
+    {
+        return strpos($this->_documentXML, '</w:tr>', $offset) + 7;
+    }
+
+    /**
+     * Get a slice of a string.
+     *
+     * @param integer $startPosition
+     * @param integer $endPosition
+     *
+     * @return string
+     */
+    protected function getSlice($startPosition, $endPosition = 0)
+    {
+        if (!$endPosition) {
+            $endPosition = strlen($this->_documentXML);
+        }
+
+        return substr($this->_documentXML, $startPosition, ($endPosition - $startPosition));
+    }
 }
 ?>
