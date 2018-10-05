414ff31bde52caa3906ed4de95b20abbf23a2511
Pavel Perminov <pavel.perminov.23@gmail.com>
2018-10-05 06:35:52 +0300

    added icons for 'pay' and 'refund' actions

--- js/admin/app/lib/controller/action/Action.js
+++ js/admin/app/lib/controller/action/Action.js
@@ -35,8 +35,9 @@ Ext.define('Indi.lib.controller.action.Action', {
          * Array of action-button aliases, that have special icons
          */
         toolbarMasterItemActionIconA: ['form', 'delete', 'save', 'toggle', 'up', 'down',
-            'print', 'm4d', 'cancel', 'php', 'author', 'login', 'confirm', 'goto', 'call']
-
+            'print', 'm4d', 'cancel', 'php', 'author', 'login', 'confirm', 'goto', 'call',
+            'pay', 'refund'
+        ]
     },
 
     /**
