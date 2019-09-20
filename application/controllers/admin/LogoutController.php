12592ebe37a67aa699445e1b917dbaefb70844c3
Pavel Perminov <pavel.perminov.23@gmail.com>
2019-09-20 03:11:39 +0300

    added cors headers for /admin/logout/

--- application/controllers/admin/LogoutController.php
+++ application/controllers/admin/LogoutController.php
@@ -7,6 +7,10 @@ class Admin_LogoutController extends Indi_Controller {
      */
     public function preDispatch(){
 
+        // Allow CORS
+        header('Access-Control-Allow-Headers: x-requested-with, indi-auth');
+        header('Access-Control-Allow-Origin: *');
+
         // Unset session
         if ($_SESSION['admin']['id'])  unset($_SESSION['admin'], $_SESSION['indi']['admin']);
 
