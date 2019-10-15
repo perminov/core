e2ac4c8c61851ea3289537ed7d549e2a1b8b3b76
Pavel Perminov <pavel.perminov.23@gmail.com>
2019-10-15 05:20:22 +0300

    1.*_Table->createRow(): added $level='model' arg for $row->compileDefaults() call 2.*_Row: added $level arg   - as 1st arg for compileDefaults($level = 'model')   - as 2nd arg for compileDefaultValue($prop, $level = 'model') 3.*_Trail_Admin->authLevel3(): added $level='trail' arg for $row->compileDefaults() call

--- library/Indi/Db/Table/Row.php
+++ library/Indi/Db/Table/Row.php
@@ -4903,12 +4903,16 @@ class Indi_Db_Table_Row implements ArrayAccess
     /**
      * This function is for compiling prop default values within *_Row instance context
      *
-     * @param $prop
+     * @param string $prop
+     * @param string $level DEFAULT 'model'
      */
-    public function compileDefaultValue($prop) {
-        if (strlen($this->_original[$prop])) {
-            Indi::$cmpTpl = $this->_original[$prop]; eval(Indi::$cmpRun); $this->$prop = Indi::cmpOut();
-        }
+    public function compileDefaultValue($prop, $level = 'model') {
+
+        // If compile expr is empty - return
+        if (!strlen($expr = $this->{$level == 'trail' ? '_modified' : '_original'}[$prop])) return;
+
+        // Compile and assign
+        Indi::$cmpTpl = $expr; eval(Indi::$cmpRun); $this->$prop = Indi::cmpOut();
     }
 
     /**
@@ -5932,9 +5936,10 @@ class Indi_Db_Table_Row implements ArrayAccess
     }
 
     /**
+     * @param string $level DEFAULT 'model'
      * @return mixed
      */
-    public function compileDefaults() {
+    public function compileDefaults($level = 'model') {
 
         // If it's an existing entry - return
         if ($this->id) return;
@@ -5946,13 +5951,13 @@ class Indi_Db_Table_Row implements ArrayAccess
             if (!$fieldR->columnTypeId) continue;
 
             // If default value should be set up as a result of php-expression's execution - do it
-            if (preg_match(Indi::rex('php'), $fieldR->defaultValue))
-                $this->compileDefaultValue($fieldR->alias);
+            if (preg_match(Indi::rex('php'), $fieldR->{$level == 'trail' ? 'modified' : 'original'}('defaultValue')))
+                $this->compileDefaultValue($fieldR->alias, $level);
 
             // Else if underlying column's datatype is TEXT - set up default value, stored in Indi
             // Engine field's settings as MySQL does not suppoer native default values for TEXT column
             else if ($fieldR->foreign('columnTypeId')->type == 'TEXT')
-                $this->compileDefaultValue($fieldR->alias);
+                $this->compileDefaultValue($fieldR->alias, $level);
         }
     }
 
